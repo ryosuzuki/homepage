289f8d62ff5c98bbef5b5beb2afac7908623856c
Ryo Suzuki <ryosuzk@gmail.com>
2019-06-23 20:50:17 -0700

    Deploy to gh-pages

---
+++ out/_next/static/webpack/static/development/pages/index.js.a7d3881c07dd6eb75917.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./content/output/activities.json":
+/*!****************************************!*\
+  !*** ./content/output/activities.json ***!
+  \****************************************/
+/*! exports provided: bodyContent, bodyHtml, title, dir, base, ext, sourceBase, sourceExt, default */
+/***/ (function(module) {
+
+module.exports = {"bodyContent":"**Organizer:** UIST 2016 (Web and Social Media Chair)\n\n**Review:** CHI 2016--2019, UIST 2017--2019, SCF 2019\n\n**Student Volunteer:** UIST 2016, CHI 2017","bodyHtml":"<p><strong>Organizer:</strong> UIST 2016 (Web and Social Media Chair)</p>\n<p><strong>Review:</strong> CHI 2016--2019, UIST 2017--2019, SCF 2019</p>\n<p><strong>Student Volunteer:</strong> UIST 2016, CHI 2017</p>\n","title":"<strong>Organizer:","dir":"content/output","base":"activities.json","ext":".json","sourceBase":"activities.md","sourceExt":".md"};
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.a7d3881c07dd6eb75917.hot-update.js.map
