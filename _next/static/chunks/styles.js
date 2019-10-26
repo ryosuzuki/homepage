15d05a95f0cc51f363b876b4ef0a7d6602f53f4c
Ryo Suzuki <ryosuzk@gmail.com>
2019-10-26 10:50:25 -0600

    Deploy to gh-pages

--- _next/static/chunks/styles.js
+++ _next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1572103695550");
+            injectCss(link, link.href.split("?")[0] + "?unix=1572104866663");
           });
       }
     }
